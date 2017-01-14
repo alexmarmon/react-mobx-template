835cbd05c90ca7cc6f8312b4592bb7c32d561bbe
Alex Marmon <aemarmon@gmail.com>
2017-01-14 11:10:39 -0800

    remove image comment

--- src/header.jsx
+++ src/header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
 import './base.scss';
-// const windTurbine = require('./images/windTurbine.svg');
 
 const Header = (({ children }) =>
   <div data-container>
