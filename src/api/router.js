67fdfb6a652eb703287d8fc3267e03c6710e8e08
Alex Marmon <aemarmon@gmail.com>
2017-01-14 12:06:12 -0800

    add knex query builder

--- src/api/router.js
+++ src/api/router.js
@@ -3,12 +3,14 @@ module.exports = (express) => {
   const mysql = require('mysql'); // eslint-disable-line global-require
 
   const creds = JSON.parse(fs.readFileSync('./creds.json'));
-  const connection = mysql.createPool({
-    host: creds.host,
-    user: creds.user,
-    password: creds.password,
-    database: creds.database,
-    conrnectionLimit: 50,
+  const knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host: creds.host,
+      user: creds.user,
+      password: creds.password,
+      database: creds.database,
+    }
   });
   const router = express.Router(); // eslint-disable-line new-cap
 
@@ -17,11 +19,10 @@ module.exports = (express) => {
   }
 
   router.route('/users').get((req, res) => {
-    connection.query('SELECT * FROM users', (err, rows) => {
-      if (err) { errHandler(err); }
+    knex.select().from('users').then((rows) => {
       const which = Math.floor((Math.random() * 9) + 0);
       res.header('Content-Type', 'application/json').status(200).send(rows[which]);
-    });
+    }).catch((err) => res.status(500).send(err));
   });
 
   return router;
