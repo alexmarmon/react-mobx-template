8643547cc9945c277e6b579d3cbbfbd0701810de
Alex Marmon <aemarmon@gmail.com>
2017-06-07 14:07:32 -0700

    fix tests, add landing page script

--- src/tests/test.jsx
+++ src/tests/test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
-import Main from '../pages/main/Main';
+import Home from '../pages/home/Home';
 import AppState from '../state/AppState';
 import server from '../../server';
 
@@ -12,18 +12,18 @@ const should = chai.should(); // eslint-disable-line no-unused-vars
 
 chai.use(chaiHttp);
 
-describe('<Main />', () => {
+describe('<Home />', () => {
   const props = new (AppState)();
 
   it('Renders the header', () => {
     expect(
-      shallow(<Main appState={props} />).find('.main'),
+      shallow(<Home appState={props} />).find('.header'),
     ).to.have.length(1);
   });
 
   it('Renders the button', () => {
     expect(
-      shallow(<Main appState={props} />).find('button'),
+      shallow(<Home appState={props} />).find('button'),
     ).to.have.length(1);
   });
 });
