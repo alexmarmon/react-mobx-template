4eefcf3dbff998fd01492c71f8799ca6b6a873e5
Alex Marmon <aemarmon@gmail.com>
2017-06-26 21:12:26 +0000

    testing husky

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,7 +10,7 @@ import User from '../../shared_modules/user/user';
 const Home = (({ appState }) =>
   <page>
     <Title />
-    <p>templateee</p>
+    <p>template</p>
     <User appState={appState} />
   </page>
 );
