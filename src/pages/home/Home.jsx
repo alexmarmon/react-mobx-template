4ab849f17fe01fa0e2a7b3ffcf94f91f404a20f5
Alex Marmon <alex@14four.com>
2017-06-27 08:40:58 -0700

    updating husky

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,7 +10,7 @@ import User from '../../shared_modules/user/user';
 const Home = (({ appState }) =>
   <page>
     <Title />
-    <p>templatecmon2</p>
+    <p>template</p>
     <User appState={appState} />
   </page>
 );
